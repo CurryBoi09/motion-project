84b919a69814eb3635865a6eabac9f55e4b8eba7
CurryBoi09 <82404864+CurryBoi09@users.noreply.github.com>
2021-04-23 13:56:15 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,30 @@
-let Car = sprites.create(img`
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.over(true)
+})
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    JumpAmount = 0
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (JumpAmount < 2) {
+        Car.vy += -100
+        JumpAmount += 1
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    Car.setPosition(5, 215)
+    pause(100)
+    info.changeLifeBy(-1)
+})
+function Introduction (text: string) {
+    game.splash("Curious George Satan Run")
+    game.splash("Help Curious George Make It to His friend Satan Before time runs out ")
+    game.splash("Collect bananas along")
+    game.splash("Use the \"A\" and \"D\" key to move and the space bar to jump")
+}
+let JumpAmount = 0
+let Car: Sprite = null
+Introduction("Introduction")
+Car = sprites.create(img`
     . . . . 2 2 2 2 2 e . . . . . . 
     . . . 2 2 2 2 d 2 2 e . . . . . 
     . . e 2 2 2 2 2 2 2 e . . . . . 
@@ -20,11 +46,9 @@ tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundColor(11)
 Car.setPosition(5, 215)
 scene.cameraFollowSprite(Car)
+JumpAmount = 0
 Car.ay = 300
+info.setLife(3)
 game.onUpdate(function () {
     Car.x += controller.dx()
-    Car.y += controller.dy()
-    if (true) {
-    	
-    }
 })
