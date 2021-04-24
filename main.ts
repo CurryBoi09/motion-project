24f58de9d9a7b99b3b1db009a8734a6c2357ca42
CurryBoi09 <82404864+CurryBoi09@users.noreply.github.com>
2021-04-24 12:12:59 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -15,33 +15,44 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sp
     pause(100)
     info.changeLifeBy(-1)
 })
+info.onCountdownEnd(function () {
+    game.over(false)
+})
 function Introduction (text: string) {
     game.splash("Curious George Satan Run")
     game.splash("Help Curious George Make It to His friend Satan Before time runs out ")
-    game.splash("Collect bananas along")
+    game.splash("Collect bananas along the way for extra points")
     game.splash("Use the \"A\" and \"D\" key to move and the space bar to jump")
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Banana`, function (sprite, location) {
+    Banana.destroy()
+    info.changeScoreBy(1)
+    music.baDing.play()
+})
 let JumpAmount = 0
+let Banana: Sprite = null
 let Car: Sprite = null
 Introduction("Introduction")
 Car = sprites.create(img`
-    . . . . 2 2 2 2 2 e . . . . . . 
-    . . . 2 2 2 2 d 2 2 e . . . . . 
-    . . e 2 2 2 2 2 2 2 e . . . . . 
-    . . e 2 2 2 2 2 2 2 e . . . . . 
-    . . e 2 2 2 2 2 e f f c c . . . 
-    . . e e 2 2 e f f f f b c . . . 
-    . e e e f e 2 b f f f d c . . . 
-    e e 2 2 d f 2 1 1 1 1 b c . . . 
-    e e 2 2 d f e e c c c . . . . . 
-    b 1 1 d e 2 2 e e c . . . . . . 
-    . f f e 2 2 2 2 e . . . . . . . 
-    . . f f d d 2 2 f f d d . . . . 
-    . . f f d d e e f f d d . . . . 
-    . . . f f f f . . . . . . . . . 
-    . . e e e f f f . . . . . . . . 
-    . . e e e e f f f . . . . . . . 
+    . . . . . . . f f f f f . . . . 
+    . . . . . . f e e e e e f . . . 
+    . . . . . f e e e d d d d f . . 
+    . . . . f f e e d f d d f d 4 . 
+    . . . f d d e e d f d d f d 4 . 
+    . . . 4 d 4 e e d d d d e e d 4 
+    f f . 4 d 4 e e d d 4 d d d d 4 
+    f e f . 4 f e e d d d 4 4 4 4 4 
+    f e f . . f f e e d d d d d f . 
+    f e f . f e e e e f f f f f . . 
+    f e f f e e e e e e e f . . . . 
+    . f f e e e e f e f f e f . . . 
+    . . f e e e e f e f f e f . . . 
+    . . . f e f f 4 d f 4 d f . . . 
+    . . . f d 4 4 d d 4 d d f . . . 
+    . . . f f f f f f f f f . . . . 
     `, SpriteKind.Player)
+Banana = assets.tile`Banana`
+Banana = 0
 tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundColor(11)
 Car.setPosition(5, 215)
@@ -49,6 +60,8 @@ scene.cameraFollowSprite(Car)
 JumpAmount = 0
 Car.ay = 300
 info.setLife(3)
+info.setScore(0)
+info.startCountdown(30)
 game.onUpdate(function () {
     Car.x += controller.dx()
 })
