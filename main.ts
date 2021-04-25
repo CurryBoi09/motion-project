5913ff75528207ce781196d7fa69d9d0341c74e4
CurryBoi09 <82404864+CurryBoi09@users.noreply.github.com>
2021-04-24 23:12:26 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,42 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    game.over(true)
+    currentLevel += 1
+    info.changeLifeBy(2)
+    music.baDing.play()
+    if (hasNextLevel()) {
+        pause(1000)
+        game.splash("Next Level")
+        setLevelTileMap(currentLevel)
+    } else {
+        game.over(true, effects.confetti)
+    }
 })
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     JumpAmount = 0
 })
+function setLevelTileMap (num: number) {
+    if (num == 0) {
+        tiles.setTilemap(tilemap`level1`)
+        Car.setPosition(5, 215)
+        info.startCountdown(50)
+    } else if (num == 1) {
+        tiles.setTilemap(tilemap`level2`)
+        Car.setPosition(5, 215)
+        info.startCountdown(50)
+    } else if (num == 2) {
+        tiles.setTilemap(tilemap`level3`)
+        Car.setPosition(5, 215)
+        info.startCountdown(70)
+    } else if (num == 3) {
+        tiles.setTilemap(tilemap`level7`)
+        Car.setPosition(5, 215)
+        info.startCountdown(60)
+    } else if (num == 4) {
+        tiles.setTilemap(tilemap`level9`)
+        Car.setPosition(5, 215)
+        info.startCountdown(70)
+    }
+    hasNextLevel()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (JumpAmount < 2) {
         Car.vy += -100
@@ -11,11 +44,17 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
-    Car.setPosition(5, 215)
+    music.smallCrash.play()
+    Car.setPosition(7, 214)
     pause(100)
     info.changeLifeBy(-1)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
+    Car.setPosition(5, 215)
+    music.zapped.play()
+})
 info.onCountdownEnd(function () {
+    music.buzzer.play()
     game.over(false)
 })
 function Introduction (text: string) {
@@ -23,14 +62,18 @@ function Introduction (text: string) {
     game.splash("Help Curious George Make It to His friend Satan Before time runs out ")
     game.splash("Collect bananas along the way for extra points")
     game.splash("Use the \"A\" and \"D\" key to move and the space bar to jump")
+    game.splash("P.S. George can use his monkey grip to grip to all sides of walls")
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`Banana`, function (sprite, location) {
-    Banana.destroy()
     info.changeScoreBy(1)
-    music.baDing.play()
+    music.beamUp.play()
 })
+function hasNextLevel () {
+    return currentLevel != levelCount
+}
+let levelCount = 0
+let currentLevel = 0
 let JumpAmount = 0
-let Banana: Sprite = null
 let Car: Sprite = null
 Introduction("Introduction")
 Car = sprites.create(img`
@@ -51,17 +94,16 @@ Car = sprites.create(img`
     . . . f d 4 4 d d 4 d d f . . . 
     . . . f f f f f f f f f . . . . 
     `, SpriteKind.Player)
-Banana = assets.tile`Banana`
-Banana = 0
-tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundColor(11)
-Car.setPosition(5, 215)
+Car.setPosition(5, 214)
 scene.cameraFollowSprite(Car)
 JumpAmount = 0
 Car.ay = 300
 info.setLife(3)
 info.setScore(0)
-info.startCountdown(30)
+currentLevel = 0
+levelCount = 5
+setLevelTileMap(currentLevel)
 game.onUpdate(function () {
     Car.x += controller.dx()
 })
